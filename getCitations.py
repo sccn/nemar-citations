90eed3f00a1c6b7e6526ece22405a1c9595dd94e
Seyed (Yahya) Shirazi <shirazi@ieee.org>
2024-01-31 20:52:23 -0800

    Revert "Neuromechanist/issue6"

--- getCitations.py
+++ getCitations.py
@@ -40,7 +40,7 @@ def get_citations(dataset: str, num_cites: int) -> pd.DataFrame:
             get_working_proxy()
             entry = scholarly.search_pubs(dataset, start_index=i)
 
-        entry = scholarly.fill(next(entry))  # This is the ith result
+        entry = next(entry)  # This is the ith result
         entry_fields = entry.keys()
         bib_fields = entry['bib'].keys()
         # Check if all expected fields are present, if not, add n/a to the dataframe
@@ -60,18 +60,11 @@ def get_citations(dataset: str, num_cites: int) -> pd.DataFrame:
             if 'pub_year' not in bib_fields:
                 entry['bib']['pub_year'] = 'n/a'
 
-        # Check the author list, replace 'and' with ';' Then, if there are more than 3 authors, replace the last
-        # 'and' with ', et al.'
-        if entry['bib']['author'] != 'n/a':
-            entry['bib']['author'] = entry['bib']['author'].replace(' and', ';')
-            if entry['bib']['author'].count(';') > 3:
-                entry['bib']['short_author'] = '; '.join(entry['bib']['author'].rsplit(';')[0:3]) + '; et al.'
-
         # Add the entry to the data frame
         citations = pd.concat([citations,
                                pd.DataFrame.from_records([
                                    {'title': entry['bib']['title'],
-                                    'author': entry['bib']['short_author'],
+                                    'author': entry['bib']['author'],
                                     'venue': entry['bib']['venue'],
                                     'year': entry['bib']['pub_year'],
                                     'url': entry['pub_url'],
